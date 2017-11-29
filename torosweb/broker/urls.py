eee7be9e230b13b84ba159bb9cd9d629add86f28
Martin Beroiz <martinberoiz@gmail.com>
2017-11-29 14:24:43 -0600

    WIP: broker

--- torosweb/broker/urls.py
+++ torosweb/broker/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^upload$', views.upload, name='upload'),
     url(r'^uploadjson$', views.uploadjson, name='uploadjson'),
-    url(r'^circular/(?P<alert_name>\w+)$', views.circular, name='circular'),
 ]
--- torosweb/broker/views.py
+++ torosweb/broker/views.py
@@ -1,61 +1,128 @@
 from django.shortcuts import render
 from .models import Assignment, Observatory, Alert, GWGCCatalog
-from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.db.models import Q
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import user_passes_test
 
 
-def index(request, the_alert=None):
-    if request.method == 'POST':
-        the_alert = Alert.objects.filter(pk=request.POST['alert_id']).get()
-        obs_id = int(request.POST['obs_id'])
-        obs = Observatory.objects.get(pk=obs_id)
+def process_assignment_form(request):
+    the_alert = Alert.objects.filter(pk=request.POST['alert_id']).get()
+    obs_id = int(request.POST['obs_id'])
+    obs = Observatory.objects.get(pk=obs_id)
 
-        obs_asg = Assignment.objects.filter(alert=the_alert)\
-            .filter(observatory=obs)
+    obs_asg = Assignment.objects.filter(alert=the_alert, observatory=obs)
 
-        aretakenlist = [int(k) for k in request.POST.getlist('istaken[]')]
-        areobservedlist = [int(k)
-                           for k in request.POST.getlist('wasobserved[]')]
+    aretakenlist = [int(k) for k in request.POST.getlist('istaken[]')]
+    areobservedlist = [int(k) for k in request.POST.getlist('wasobserved[]')]
 
-        print(aretakenlist)
+    def taken_by_others(asg):
+        other_assng = Assignment.objects.filter(alert=the_alert)\
+            .exclude(observatory=asg.observatory)\
+            .filter(is_taken=True, target=asg.target)
+        return other_assng.exists()
 
-        def taken_by_others(asg):
-            other_assng = Assignment.objects.filter(alert=the_alert)\
-                .exclude(observatory=asg.observatory)\
-                .filter(is_taken=True)\
-                .filter(target=asg.target)
-            return other_assng.exists()
+    def turn_on_taken(asg):
+        if asg.is_taken is False:
+            asg.is_taken = True
 
-        def turn_on_taken(asg):
-            if asg.is_taken is False:
-                asg.is_taken = True
+    def turn_off_taken(asg):
+        if asg.is_taken is True:
+            asg.is_taken = False
 
-        def turn_off_taken(asg):
-            if asg.is_taken is True:
-                asg.is_taken = False
-
-        for asg in obs_asg:
-            if asg.id in aretakenlist:
-                if not taken_by_others(asg) \
-                        or asg.id in areobservedlist:
-                    turn_on_taken(asg)
-                else:
-                    turn_off_taken(asg)
-                    # Change mode to taken
+    for asg in obs_asg:
+        if asg.id in aretakenlist:
+            if not taken_by_others(asg) \
+                    or asg.id in areobservedlist:
+                turn_on_taken(asg)
             else:
                 turn_off_taken(asg)
-            if asg.id in areobservedlist:
-                if asg.was_observed is False:
-                    asg.was_observed = True
-            else:
-                if asg.was_observed is True:
-                    asg.was_observed = False
-            asg.save()
+                # Change mode to taken
+        else:
+            turn_off_taken(asg)
+        if asg.id in areobservedlist:
+            if asg.was_observed is False:
+                asg.was_observed = True
+        else:
+            if asg.was_observed is True:
+                asg.was_observed = False
+        asg.save()
 
+
+def process_upload_target_form(request):
+    from django.utils import timezone
+
+    assgn_text = request.POST.get('assignments', '')
+    alert_id = request.POST.get('alert', None)
+    thealert = Alert.objects.get(pk=alert_id)
+
+    was_error = False
+    error_msg = []
+
+    if not request.user.groups.filter(name='broker_admins').exists():
+        error_msq = ["You don't have enough permissions to upload targets."]
+        return 403, error_msg
+
+    obss = filter(None, assgn_text.split(';'))
+    for anobs_text in obss:
+        split = anobs_text.split(':')
+        try:
+            obs, obj_text = split
+        except:
+            was_error = True
+            error_msg.append("Error parsing line: {}".format(anobs_text))
+            continue
+        obs = obs.strip()
+        name_lookup = Q(name__contains=obs) | Q(short_name__contains=obs)
+        try:
+            theobs = Observatory.objects.get(name_lookup)
+        except:
+            was_error = True
+            error_msg.append("Could not find observatory {}".format(obs))
+            continue
+        objs = [obj.strip() for obj in filter(None, obj_text.split(','))]
+        for obj_prob in objs:
+            try:
+                obj_prob_split = filter(None, obj_prob.split())
+                if len(obj_prob_split) == 1:
+                    obj = obj_prob_split[0]
+                    prob = 0.0
+                else:
+                    obj, prob = obj_prob_split
+                    prob = float(prob)
+            except:
+                was_error = True
+                error_msg.append("Could not parse '{}'".format(obj_prob))
+                continue
+            try:
+                theobj = GWGCCatalog.objects.get(name=obj)
+            except:
+                was_error = True
+                error_msg.append("Could not find object {}".format(obj))
+                continue
+            new_assgn = Assignment(
+                target=theobj, observatory=theobs,
+                alert=thealert, datetime=timezone.now(), probability=prob)
+            new_assgn.save()
+
+    return (200, error_msg) if not error_msg else (400, error_msg)
+
+
+def has_broker_access(user):
+    return user.groups.filter(name__in=['broker_admins',
+                                        'telescope_operators'])
+
+
+@user_passes_test(has_broker_access)
+def index(request, the_alert=None):
     context = {}
+    status = 200
+    if request.method == 'POST' and 'upload_target' in request.POST:
+        status, context['errors'] = process_upload_target_form(request)
+    if request.method == 'POST' and 'upload_target' not in request.POST:
+        status, context['errors'] = process_assignment_form(request)
+
     context['alerts'] = Alert.objects.order_by('-datetime')
 
     if the_alert is None:
@@ -63,29 +130,26 @@ def index(request, the_alert=None):
     context['the_alert'] = the_alert
 
     context['all_assingments'] = Assignment.objects\
-        .filter(alert=the_alert)\
-        .filter(is_taken=True)\
+        .filter(alert=the_alert, is_taken=True)\
         .order_by('target__name')
 
-    selected_targets = Assignment.objects.filter(alert=the_alert)\
-        .filter(is_taken=True).count()
+    selected_targets = Assignment.objects.filter(
+        alert=the_alert, is_taken=True).count()
     context['selected_targets'] = selected_targets
 
-    observed_targets = Assignment.objects.filter(alert=the_alert)\
-        .filter(was_observed=True).count()
+    observed_targets = Assignment.objects.filter(
+        alert=the_alert, was_observed=True).count()
     context['observed_targets'] = observed_targets
 
     def taken_by_others(asg):
         other_assng = Assignment.objects.filter(alert=the_alert)\
             .exclude(observatory=asg.observatory)\
-            .filter(is_taken=True)\
-            .filter(target=asg.target)
+            .filter(is_taken=True, target=asg.target)
         return other_assng.exists()
 
     assn_per_obs = []
     for obs in Observatory.objects.all():
-        assgnms = Assignment.objects.filter(alert=the_alert)\
-                                    .filter(observatory=obs)
+        assgnms = Assignment.objects.filter(alert=the_alert, observatory=obs)
         for asg in assgnms:
             if (not asg.is_taken) and taken_by_others(asg):
                 asg.flag_unavailable = True
@@ -96,67 +160,7 @@ def index(request, the_alert=None):
     context['is_admin'] = request.user.groups\
         .filter(name='broker_admins').exists()
 
-    return render(request, 'broker/index.html', context)
-
-
-def upload(request):
-    from django.utils import timezone
-
-    context = {}
-    context['alerts'] = Alert.objects.order_by('-datetime')
-
-    if request.method == 'POST':
-        assgn_text = request.POST.get('assignments', '')
-        alert_id = request.POST.get('alert', None)
-        thealert = Alert.objects.get(pk=alert_id)
-
-        was_error = False
-        error_msg = []
-        obss = filter(None, assgn_text.split(';'))
-        for anobs_text in obss:
-            split = anobs_text.split(':')
-            try:
-                obs, obj_text = split
-            except:
-                was_error = True
-                error_msg.append("Error parsing line: {}".format(anobs_text))
-                continue
-            obs = obs.strip()
-            name_lookup = Q(name__contains=obs) | Q(short_name__contains=obs)
-            try:
-                theobs = Observatory.objects.get(name_lookup)
-            except:
-                was_error = True
-                error_msg.append("Could not find observatory {}".format(obs))
-                continue
-            objs = [obj.strip() for obj in filter(None, obj_text.split(','))]
-            for obj_prob in objs:
-                try:
-                    obj_prob_split = filter(None, obj_prob.split())
-                    if len(obj_prob_split) == 1:
-                        obj = obj_prob_split[0]
-                        prob = 0.0
-                    else:
-                        obj, prob = obj_prob_split
-                        prob = float(prob)
-                except:
-                    was_error = True
-                    error_msg.append("Could not parse '{}'".format(obj_prob))
-                    continue
-                try:
-                    theobj = GWGCCatalog.objects.get(name=obj)
-                except:
-                    was_error = True
-                    error_msg.append("Could not find object {}".format(obj))
-                    continue
-                new_assgn = Assignment(
-                    target=theobj, observatory=theobs,
-                    alert=thealert, datetime=timezone.now(), probability=prob)
-                new_assgn.save()
-        if was_error:
-            context['errors'] = error_msg
-
-    return render(request, 'broker/upload.html', context)
+    return render(request, 'broker/index.html', context, status=status)
 
 
 @require_POST
@@ -191,24 +195,4 @@ def uploadjson(request):
                 new_assgn.save()
     except:
         return HttpResponseBadRequest()
-
     return HttpResponse()
-
-
-def circular(request, alert_name):
-    context = {}
-
-    the_alert = Alert.objects.filter(grace_id=alert_name).first()
-    if the_alert is None:
-        the_alert = Alert.objects.order_by('-datetime').first()
-    context['alert'] = the_alert
-
-    observed_per_obs = []
-    for obs in Observatory.objects.all():
-        observed_per_obs.append([obs,
-                                 Assignment.objects.
-                                 filter(alert=the_alert).
-                                 filter(observatory=obs).
-                                 filter(was_observed=True)])
-    context['observed_per_obs'] = observed_per_obs
-    return render(request, 'broker/circular.html', context)
