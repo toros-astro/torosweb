d18b6ac3ae6a29b078dea57ab63aab0741bff969
Martin Beroiz <martinberoiz@gmail.com>
2017-12-13 13:55:58 -0600

    Added migration for winnow and account

---
+++ torosweb/config/local_settings_debug.py
@@ -0,0 +1,12 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'dev_db.sqlite3'),
+    }
+}
+
+ALLOWED_HOSTS = ['*']
