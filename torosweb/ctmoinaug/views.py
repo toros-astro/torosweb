b2c3415f2b8246d2ba2408a8e867509495659d95
Martin Beroiz <martinberoiz@gmail.com>
2018-04-12 15:55:05 -0500

    moved mcastillo to group permissions

--- torosweb/ctmoinaug/views.py
+++ torosweb/ctmoinaug/views.py
@@ -10,6 +10,9 @@ def activity_detail(request):
             form = ActivityForm()
     else:
         form = ActivityForm()
+    can_see_info = request.user.groups.filter(name='ctmo_admin').exists()
+    print(can_see_info)
     activities = Activity.objects.all()
     return render(request, 'ctmoinaug/activities.html', 
-                  {'activities': activities, 'form': form})
+                  {'activities': activities, 'form': form,
+                  'do_disclose': can_see_info})
