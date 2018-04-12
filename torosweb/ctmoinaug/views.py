1b8f0e05e17c8160e4e3a2813459ad68252b9fd3
Martin Beroiz <martinberoiz@gmail.com>
2018-04-12 14:08:27 -0500

    Added form to enter new values

--- torosweb/ctmoinaug/views.py
+++ torosweb/ctmoinaug/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from .models import Activity
-
+from .forms import ActivityForm
 
 def activity_detail(request):
+    if request.method == "POST":
+        form = ActivityForm(request.POST) 
+        if form.is_valid():
+            activity = form.save()
+            form = ActivityForm()
+    else:
+        form = ActivityForm()
     activities = Activity.objects.all()
     return render(request, 'ctmoinaug/activities.html', 
-                  {'activities': activities, })
-    
+                  {'activities': activities, 'form': form})
