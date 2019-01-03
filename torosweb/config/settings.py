725dfa731f9bde0ba0540c742024b3192de23261
Martin Beroiz <martinberoiz@gmail.com>
2019-01-03 16:09:40 -0600

    Bumped version

--- torosweb/config/settings.py
+++ torosweb/config/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
-__version__ = '1.0'
+__version__ = '1.1'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
